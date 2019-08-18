7f4580d7affbbc578cdff389c88dbb695449d198
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-08-18 13:00:32 -0400

    Default layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DefaultComponent } from './layout/default/default.component';
+
+const routes: Routes = [
+    {
+        path: ``,
+        component: DefaultComponent,
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
