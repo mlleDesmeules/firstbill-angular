9703657f113f6d4cce499e5b5b5e094e9b5298f8
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-08-19 20:08:10 -0400

    Update detail form to contain frequency

--- src/app/modules/bills/components/form/form.component.ts
+++ src/app/modules/bills/components/form/form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 import { Bill } from '@core/models/bills/bill.model';
+import { FREQUENCIES, FREQUENCY_MONTHLY } from '@core/models/bills/frequency.model';
 import { BillService } from '@core/services/bills/bill.service';
 
 @Component({
@@ -12,22 +13,42 @@ import { BillService } from '@core/services/bills/bill.service';
 export class FormComponent implements OnInit {
 
     public form;
+    public frequencyList = [];
 
-    constructor(private service: BillService) { }
+    constructor(private builder: FormBuilder,
+                private service: BillService) { }
 
     ngOnInit() {
         this._createForm();
+        this._getFrequencyList();
     }
 
     private _createForm() {
-        this.form = new FormGroup({
-            name: new FormControl(),
-            dueDate: new FormControl(),
+        this.form = this.builder.group({
+            name     : [`Test`],
+            amount   : [`120`],
+            frequency: [ FREQUENCY_MONTHLY ],
+            dueDate  : [`2019/09/02`],
         });
     }
 
+    private _getFrequencyList() {
+        this.frequencyList = FREQUENCIES;
+
+        this.frequencyList.sort((a, b) => Number(a.order > b.order));
+    }
+
     public save() {
-        const bill = new Bill(this.form.getRawValue());
+        const data = {
+            name: this.form.get(`name`).value,
+            amount: this.form.get(`amount`).value,
+            dueDate: this.form.get(`dueDate`).value,
+            frequency: this.form.get(`frequency`).value,
+        };
+
+        console.log(data);
+
+        const bill = new Bill(data);
 
         this.service.add(bill);
     }
