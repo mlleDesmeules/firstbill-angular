39d09afc11582e87180c332f869881fb7dc72e22
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-08-20 20:31:48 -0400

    Allow form reset & add comments

--- src/app/modules/bills/components/form/form.component.ts
+++ src/app/modules/bills/components/form/form.component.ts
@@ -23,6 +23,12 @@ export class FormComponent implements OnInit {
         this._getFrequencyList();
     }
 
+    /**
+     * Create form
+     *
+     * This method will create the form object and set all of its possible attributes, with the
+     * appropriate values as default (when necessary).
+     */
     private _createForm() {
         this.form = this.builder.group({
             name     : [``],
@@ -32,12 +38,25 @@ export class FormComponent implements OnInit {
         });
     }
 
+    /**
+     * Get frequency list
+     *
+     * This method will get the list of possible frequencies and sort them in the display
+     * order.
+     */
     private _getFrequencyList() {
         this.frequencyList = FREQUENCIES;
 
         this.frequencyList.sort((a, b) => Number(a.order > b.order));
     }
 
+    /**
+     * Save form
+     *
+     * This method will get the form's data, create an Bill object with it and then calling the
+     * bill service to save the Bill. The Bill model is created before sending the object to
+     * make sure that all of the attributes that can have default values have them.
+     */
     public save() {
         const data = {
             name: this.form.get(`name`).value,
@@ -46,10 +65,18 @@ export class FormComponent implements OnInit {
             frequency: this.form.get(`frequency`).value,
         };
 
-        console.log(data);
-
         const bill = new Bill(data);
 
         this.service.add(bill);
+    }
+
+    /**
+     * Reset form
+     *
+     * This method will call the method creating the form to reset all of it's value to what it
+     * was on page load.
+     */
+    public reset() {
+        this._createForm();
     }
 }
