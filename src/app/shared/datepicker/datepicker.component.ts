01f1b6a1b8be21fb2155173bca6a752b78334176
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-09-04 21:23:28 -0400

    homemade bulma calendar component
     Bulma calendar library doesn't work with angular, so I'm working on an
     angular component that will work and look like the Bulma calendar.

---
+++ src/app/shared/datepicker/datepicker.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+@Component({
+	selector   : `app-datepicker`,
+	templateUrl: `./datepicker.component.html`,
+	styleUrls  : [ `./datepicker.component.scss` ],
+})
+export class DatepickerComponent implements OnInit {
+
+	selectedDate: any;
+	navDate: any;
+
+	constructor() { }
+
+	ngOnInit() {
+		this.navDate = moment();
+		this.selectedDate = moment();
+
+		this.getDays();
+	}
+
+	changeNavMonth(num: number) {
+		this.navDate.add(num, `month`);
+	}
+
+	getDays(): any[] {
+		const firstDay  = moment(this.navDate).startOf(`month`);
+		const lastDay   = moment(this.navDate).endOf(`month`);
+
+		const startEmpty = firstDay.weekday();
+		const lastEmpty  = 6 - lastDay.weekday();
+
+		const gridSize = startEmpty + lastEmpty + moment(this.navDate).daysInMonth();
+
+		const list = [];
+
+		for (let i = 0; i < gridSize; i++) {
+			let day = null;
+
+			if (i < startEmpty || i >= gridSize - lastEmpty) {
+				day = null;
+			} else {
+				day = (i - startEmpty + 1);
+			}
+
+			list.push({
+				value    : day,
+				available: (day !== null),
+				fullDate : moment(moment(this.navDate).date(day)).toISOString(),
+			});
+		}
+
+		return list;
+	}
+
+	getWeekdays(): string[] {
+		const list: number[]   = [0, 1, 2, 3, 4, 5, 6];
+		const result: string[] = [];
+
+		list.forEach((day) => {
+			result.push(moment().weekday(day).format(`ddd`));
+		});
+
+		return result;
+	}
+
+	isSelected(date: string): boolean {
+		return (date === moment(this.selectedDate).toISOString());
+	}
+
+	selectDate(date: string) {
+	  	this.selectedDate = moment(date);
+	}
+}
